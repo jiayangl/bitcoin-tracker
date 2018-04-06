911567053f65f4a4e11793c45240c079dc522ca3
Jiayang Li <gscsdyang@gmail.com>
2018-04-06 12:05:29 -0400

    style

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+const palette = {
+    white: '#ffffff',
+    offwhite: '#F7F8FE',
+    gray: '#DCDCE3',
+    teal: '#4FCACF',
+    lightBlue: '#55A0F8',
+    darkBlue: '#4756B5',
+    black: '#31323B'
+};
